46bd34b79483c3e103309b47ac7141ac7a27031e
elijah obara <elijahobara357@gmail.com>
2023-02-15 13:38:53 +0300

    Configured db and .env file

--- app/main.py
+++ app/main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
 
-from . import config
-settings = config.get_settings()
+#from . import config
 
 app = FastAPI()
+ # settings = config.get_settings()
+
 
 @app.get("/")
 def homepage():
-    return {"hello": "world", "keyspace": settings.keyspace}
+    return {"hello": "world"}
 
