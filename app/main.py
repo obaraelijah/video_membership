6db4a637c7b0b70242cb08b1ff9d02191a3447aa
elijah obara <elijahobara357@gmail.com>
2023-02-16 19:44:04 +0300

    Added login and signup endpoints and added html templates

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 import pathlib
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from .users.models import User
@@ -35,6 +35,42 @@ def homepage(request: Request):
         "abc": 123
     }
     return templates.TemplateResponse("home.html", context)
+
+
+@app.get("/login", response_class=HTMLResponse)
+def login_get_view(request: Request):
+
+    return templates.TemplateResponse("auth/login.html", {
+        "request": request,
+    })
+
+@app.post("/login", response_class=HTMLResponse)
+def login_post_view(request: Request,
+    email: str=Form(...),
+    password: str = Form(...)):
+    print(email, password)
+    return templates.TemplateResponse("auth/login.html", {
+        "request": request,
+    })
+    
+
+@app.get("/signup", response_class=HTMLResponse)
+def signup_get_view(request: Request):
+
+    return templates.TemplateResponse("auth/signup.html", {
+        "request": request,
+    })
+
+@app.post("/signup", response_class=HTMLResponse)
+def signup_post_view(request: Request,
+    email: str=Form(...),
+    password: str = Form(...),
+    password_confirm: str = Form(...)
+    ):
+    return templates.TemplateResponse("auth/signup.html", {
+        "request": request,
+    })
+    
 
 @app.get("/users")
 def users_list_view():
