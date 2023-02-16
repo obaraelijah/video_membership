96cf4222c7608c57b60124e7591c5528a98803e2
elijah obara <elijahobara357@gmail.com>
2023-02-16 15:13:26 +0300

    Configured jinja for html templates and fixed some issues in my code

--- app/main.py
+++ app/main.py
@@ -1,12 +1,23 @@
-from fastapi import FastAPI
-
+import pathlib
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 from .users.models import User
 from . import config, db
 from cassandra.cqlengine.management import sync_table
 
+
+BASE_DIR = pathlib.Path(__file__).resolve().parent #app/
+TEMPLATE_DIR = BASE_DIR / "templates"
+
+
 app = FastAPI()
+templates = Jinja2Templates(directory=str(TEMPLATE_DIR))
+
+
 DB_SESSION = None
  # settings = config.get_settings()
+
 
 
 @app.on_event("startup")
@@ -17,9 +28,13 @@ def on_startup():
     DB_SESSION = db.get_session()
     sync_table(User)
     
-@app.get("/")
-def homepage():
-    return {"hello": "world"} # json data -> REST API
+@app.get("/", response_class=HTMLResponse)
+def homepage(request: Request):
+    context = {
+        "request": request,
+        "abc": 123
+    }
+    return templates.TemplateResponse("home.html", context)
 
 @app.get("/users")
 def users_list_view():
