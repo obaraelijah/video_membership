4f04c92d8bf5e4cf35c443ae367568a3d89239f3
elijah obara <elijahobara357@gmail.com>
2023-02-16 09:08:16 +0300

    Implimented the hash method to the  password and security features

--- app/users/models.py
+++ app/users/models.py
@@ -2,9 +2,11 @@ import uuid
 from cassandra.cqlengine  import columns
 from cassandra.cqlengine.models import Model
 
+
+
 from app.config import get_settings
 
-from . import validators
+from . import validators,security
 
 settings = get_settings()
 
@@ -22,6 +24,19 @@ class User(Model):
     def __repr__(self):
         return f"User(email={self.email}, user_id={self.user_id})"
     
+    def set_password(self, pw, commit=False):
+        pw_hash =security.generate_hash(pw)
+        self.password = pw_hash
+        if commit:
+            self.save()
+        return True
+    
+    def verify_password(self, pw_str):
+        pw_hash = self.password
+        verified, _ = security.verify_hash(pw_hash, pw_str)
+        return verified
+        
+    
     @staticmethod
     def create_user(email, password=None):
         q = User.objects.filter(email=email)
@@ -31,6 +46,7 @@ class User(Model):
         if not valid:
             raise  Exception(f"Invalid email: {msg}") 
         obj = User(email=email)
-        obj.password = password
+        obj.set_password(password)
+        #obj.password = password
         obj.save()
         return obj
