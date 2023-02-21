538c3c2b20c5e24d3c3b37e4aea2254666064fe9
elijah obara <elijahobara357@gmail.com>
2023-02-21 12:56:24 +0300

    Youtube video id extraction

--- app/videos/models.py
+++ app/videos/models.py
@@ -1,10 +1,13 @@
 import uuid
 from cassandra.cqlengine  import columns
+from app.users.models import User
 from cassandra.cqlengine.models import Model
 from app.config import get_settings
 
+from .extractors import extract_video_id
 
 settings = get_settings()
+
 
 class Video(Model):
     __keyspace__ = settings.keyspace
@@ -25,4 +28,23 @@ class Video(Model):
     
     @staticmethod
     def add_video(url, user_id=None):
-        pass  
+        # extract video_id from url
+        # video_id = host_id
+        # service API - Youtube/vimeo ...
+        host_id = extract_video_id(url)
+        if host_id is None:
+            raise Exception("Invalid Youtube Video URL")
+        user_id  = User.check_exists(user_id)
+        if user_id is None:
+            raise Exception("Invalid user_id")
+        # user_obj = User.by_user_id(user_id)
+        # user_obj.display_name
+        q = Video.objects.filter(host_id=host_id, user_id=user_id)
+        if q.count() != 0:
+            raise Exception("Video already added")
+        return Video.create(host_id=host_id, user_id=user_id, url=url)
+    
+    
+    
+   # class PrivateVideo(Video):
+   #     pass
