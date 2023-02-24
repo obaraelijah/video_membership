4b92473a010c885aa52d91c7ccd6d63223e608fb
elijah obara <elijahobara357@gmail.com>
2023-02-24 13:38:47 +0300

    Implimented dynamic url routing

--- app/videos/routers.py
+++ app/videos/routers.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Request,Form
 from fastapi.responses import HTMLResponse
 
 from app.users.decorators import login_required
-from app.shortcuts import render, redirect
+from app.shortcuts import render, redirect, get_object_or_404
 from app import utils
 
 from .schemas import VideoCreateSchema
@@ -48,8 +48,15 @@ def video_list_view(request: Request):
     }
     return render(request, "videos/list.html", context)
 
+# host_id ='video-1'
+# f"{host_id} is cool"
 
-@router.get("/detail", response_class=HTMLResponse)
-def video_detail_view(request: Request):
-    return render(request, "videos/detail.html", {})
+@router.get("/{host_id}", response_class=HTMLResponse)
+def video_detail_view(request: Request, host_id: str):
+    obj = get_object_or_404(Video, host_id=host_id)
+    context = {
+        "host_id": host_id,
+        "object": obj
+    }
+    return render(request, "videos/detail.html", context)
  
