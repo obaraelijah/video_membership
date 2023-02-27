2d4776b6c6bf14ea9da0ccc37091bbf3cd0b20a6
elijah obara <elijahobara357@gmail.com>
2023-02-27 10:57:20 +0300

    Fetching data to endpoint and converting data to json data

--- app/main.py
+++ app/main.py
@@ -111,3 +111,13 @@ def signup_post_view(request: Request,
 def users_list_view():
     q =User.objects.all().limit(10)
     return list(q)
+
+#import json
+# json.dumps()
+#json.loads(jsnon_str) -> dict
+
+@app.post("/watch-event")
+def watch_event_view(request:Request, data:dict):
+    print(data)
+    print(request.user.is_authenticated)
+    return {"working": True}
