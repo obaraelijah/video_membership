7b48c3ac68e21188e1d793664a43087ca09596d3
elijah obara <elijahobara357@gmail.com>
2023-03-02 09:18:43 +0300

    Created playlist model and handled exceptions due to errors

--- app/shortcuts.py
+++ app/shortcuts.py
@@ -3,12 +3,16 @@ from cassandra.cqlengine.query import (DoesNotExist, MultipleObjectsReturned)
 
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse ,RedirectResponse
-
+from fastapi import Request
 
 from starlette.exceptions import HTTPException as StarletteHTTPException
 
 settings = config.get_settings()
 templates = Jinja2Templates(directory=str(settings.templates_dir))
+
+
+def is_htmx(request: Request):
+    return request.headers.get("hx-request") == 'true'
 
 
 def get_object_or_404(KlassName, **kwargs):
